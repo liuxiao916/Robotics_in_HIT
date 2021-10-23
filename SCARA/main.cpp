d83ba74eba085c28d25326b6a9479d552d346ff2
Liu,Xiao <45966993+liuxiao916@users.noreply.github.com>
2021-10-23 19:22:20 +0800

    Update main.cpp

--- SCARA/main.cpp
+++ SCARA/main.cpp
@@ -8,9 +8,19 @@ int main(int, char**) {
     double x, y ,z ,roll ,p, yaw;
     double joint[4]={0};
     Matrix<double, 3, 4> q;
-    SRobot::SetRobotEndPos(415.402,196.478,-17.354,0,180,-149.424);
+    //Robot::SetRobotEndPos(415.402,196.478,-17.354,0,180,-149.424);
     //SRobot::SetRobotEndPos(213.430,-0.136,-17.354,0,180,-165.577);
+    SRobot::SetRobotEndPos(409.694, -167.773, -17.354, 0, 180, -106.310);
     SRobot::GetJointAngles(joint[0],joint[1],joint[2],joint[3]);
-    //cout << x <<" " << y<<" " <<z;
+    cout << joint[0] <<" " << joint[1] <<" " <<joint[2]<<" "<< joint[3]<<endl ;
 
+    SRobot::SetRobotJoint(2.101, 46.426, 17.354, 79.111);
+    SRobot::GetJointEndPos(x,y,z,roll,p,yaw);
+    cout << x <<" " << y <<" " <<z<<" "<< roll<<" "<<p<<" "<<yaw <<endl;
+    SRobot::SetRobotJoint(-49.965, 55.391, 17.354, 79.116);
+    SRobot::GetJointEndPos(x,y,z,roll,p,yaw);
+    cout << x <<" " << y <<" " <<z<<" "<< roll<<" "<<p<<" "<<yaw <<endl;
+    SRobot::SetRobotJoint(-64.768, 129.463, 17.354, 79.118);
+    SRobot::GetJointEndPos(x,y,z,roll,p,yaw);
+    cout << x <<" " << y <<" " <<z<<" "<< roll<<" "<<p<<" "<<yaw <<endl;
 }
